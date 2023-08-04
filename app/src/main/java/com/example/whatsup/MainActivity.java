12ec55b01a55ec702ce90ea2abc3ed76794c29eb
sakilAhmed <sakil390.28@gmail.com>
2023-08-04 21:02:56 +0600

    Second commit

--- app/src/main/java/com/example/whatsup/MainActivity.java
+++ app/src/main/java/com/example/whatsup/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int I_can_hear_you = 0;
        // TODO : complete this
     }
 }
