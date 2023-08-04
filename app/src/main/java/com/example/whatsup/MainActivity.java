2a61f94caec56bf226368b533e41a8787df2c2b4
sakilAhmed <sakil390.28@gmail.com>
2023-08-04 20:58:55 +0600

    First Commit

---
+++ app/src/main/java/com/example/whatsup/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.whatsup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       // TODO : complete this
+    }
+}
